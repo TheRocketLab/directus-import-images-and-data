b4c0cb97c3aa4f299f576fa4b9eda4579708352e
Davin McDonald <davin.mcdonald@rocketlab.com.au>
2023-07-10 16:21:14 +1000

    add upload json data functions, add git ignore, add readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # directus-import-images-and-data
-This repo allows bulk upload images and associate them with a collection in a single import
+This repo allows bulk upload images and associate them with a collection in a single import. 
+
+## Steps to run
+1. Run `npm i` in terminal
+2. Export your data to `data-sample.json` making sure the object looks like the existing
+3. Copy the `.env.sample` file to `.env` and fill in using your Directus credentials
+4. Check the `uploadJsonData.js` as you may need to adjust some values to suit your requirements
+5. Run `node uploadJsonData.js` to run the import
---
+++ uploadJsonData.js
@@ -0,0 +1,111 @@
+require('dotenv').config()
+const fs = require('fs')
+const FormData = require('form-data');
+
+// Read data from JSON file
+const data = fs.readFileSync('data-sample.json', 'utf-8');
+const jsonData = JSON.parse(data);
+
+const username = process.env.DIRECTUS_USERNAME;
+const password = process.env.DIRECTUS_PASSWORD;
+const endpoint = process.env.DIRECTUS_API_ENDPOINT;
+// Replace this with the name of your collection
+const collection = 'blog_articles';
+
+async function authenticate(username, password, endpoint) {
+  const { default: fetch } = await import('node-fetch');
+  const authUrl = `${endpoint}/auth/login`;
+  const response = await fetch(authUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: username,
+      password: password
+    })
+  });
+  const responseJson = await response.json();
+  if (responseJson.data) {
+    return responseJson.data.access_token;
+  } else {
+    throw new Error(`Authentication failed: ${responseJson.toString()}`);
+  }
+}
+
+async function uploadImageFromUrl(token, endpoint, slug, imageUrl) {
+  const { default: fetch } = await import('node-fetch');
+  const uploadUrl = `${endpoint}/files`;
+  
+  // Download the image from the URL
+  const imageResponse = await fetch(imageUrl);
+  const imageBuffer = await imageResponse.buffer();
+
+  // Create form data with the image blob
+  const formData = new FormData();
+  formData.append('file', imageBuffer, { filename: slug + '.jpg' });
+
+  // Upload the image to Directus
+  const response = await fetch(uploadUrl, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+    body: formData
+  });
+
+  const responseJson = await response.json();
+  console.log('responseJson', responseJson)
+  if (responseJson.data) {
+    const fileId = responseJson.data.id;
+
+    return fileId;
+
+  } else {
+    throw new Error(`Upload failed: ${responseJson.error.message}`);
+  }
+}
+
+async function uploadJsonData(token, endpoint, collection, jsonData) {
+  const { default: fetch } = await import('node-fetch');
+  const uploadUrl = `${endpoint}/items/${collection}`;
+  const response = await fetch(uploadUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(jsonData)
+  });
+  const responseJson = await response.json();
+  if (responseJson.data) {
+    return responseJson.data;
+  } else {
+    throw new Error(`Upload failed: ${responseJson.error.message}`);
+  }
+}
+
+(async () => {
+  try {
+    // Authenticate and get the access token
+    const token = await authenticate(username, password, endpoint);
+
+    jsonData.map(async (item) => {
+      // Upload the image to Directus
+      const imageId = await uploadImageFromUrl(token, endpoint, item.slug, item.feature_image);
+      // map the returned image UID to the feature_image property
+      const jsonDataWithImage = [{
+        ...item,
+        feature_image: imageId
+      }]
+
+      // Upload the JSON data to the collection
+      const uploadedData = await uploadJsonData(token, endpoint, collection, jsonDataWithImage);
+      console.log('uploadedData', uploadedData);
+    });
+
+    console.log('Data uploaded successfully!');
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+})();
